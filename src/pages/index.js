bc3504726c292435c3eae2d676760dea0fb9082d
ManhDang <89623168+NightFury2s@users.noreply.github.com>
2024-04-10 15:12:38 +0700

    Update UI for HomePage (ProductCard)

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,9 @@ export default function Home() {
                         <div className="w-full h-auto bg-white shadow-2xl rounded-lg p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
                             {motorbikeProducts &&
                                 motorbikeProducts.map((product, index) => (
-                                    <ProductCard key={index} product={product} />
+                                    <Link key={index} href={`/DetailProduct?id=${product.id}`}>
+                                        <ProductCard product={product} />
+                                    </Link>
                                 ))}
                         </div>
                     </div>
@@ -70,11 +72,13 @@ export default function Home() {
                             </Link>
                         </div>
                     </div>
-                    <div className="p-5 mb-10">
+                    <div className="p-5 mb-20">
                         <div className="w-full h-auto bg-white shadow-2xl rounded-lg p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
                             {accessoriesProducts &&
                                 accessoriesProducts.map((product, index) => (
-                                    <ProductCard key={index} product={product} />
+                                    <Link key={index} href={`/DetailProduct?id=${product.id}`}>
+                                        <ProductCard product={product} />
+                                    </Link>
                                 ))}
                         </div>
                     </div>
