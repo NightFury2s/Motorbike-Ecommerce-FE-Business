2ec8047b503ae15076caa05e5b66b082f6146403
ManhDang <89623168+NightFury2s@users.noreply.github.com>
2024-04-11 17:06:41 +0700

    UI for ProfilePage

--- src/components/constants/Input/index.js
+++ src/components/constants/Input/index.js
@@ -1,140 +1,161 @@
-import PropTypes from "prop-types";
-import { IoEyeOff } from "react-icons/io5";
+import PropTypes from 'prop-types';
+import { IoEyeOff } from 'react-icons/io5';
 import { useState, useRef } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaPencilAlt } from 'react-icons/fa';
+
 function InputComponent({
-  type,
-  placeholder,
-  icon,
-  textMessage,
-  register,
-  name,
-  errors,
-  pattern,
-  passwordsMatch,
-  minLength,
-  typePassword,
-  severErorEmail,
-  severErorPhone,
-  severErorUser,
-  handleReset,
-  ...props
+    type,
+    placeholder,
+    icon,
+    textMessage,
+    register,
+    name,
+    errors,
+    pattern,
+    passwordsMatch,
+    minLength,
+    typePassword,
+    severErorEmail,
+    severErorPhone,
+    severErorUser,
+    handleReset,
+    ...props
 }) {
-  const [eye, SetEye] = useState(false);
+    const [eye, SetEye] = useState(false);
 
-  const checkErr=()=>{
-      if(errors[name] || severErorEmail  || severErorUser || severErorUser ||passwordsMatch ){
-        return 'red'
-      }
-      else{
-        return 'transparent'
-      }
-  }
+    const checkErr = () => {
+        if (errors[name] || severErorEmail || severErorUser || severErorUser || passwordsMatch) {
+            return 'red';
+        } else {
+            return 'transparent';
+        }
+    };
 
+    const handleEye = () => {
+        SetEye(!eye);
+    };
 
-  const handleEye = () => {
-    SetEye(!eye);
-  };
-
-  return (
-    <>
-      <div style={{ alignItems: "center" , border:`0.5px solid ${checkErr()}` }} className="bg-gray-100 p-2 flex">
-        {icon}
-        <input
-          id="input"
-          name={name}
-          type={typePassword ? (eye ? "text" : "password") : "text"}
-          placeholder={placeholder}
-          onInput={()=>{handleReset? handleReset():null}}
-          className="w_64 bg_transparent bg-gray-100 outline-none text-sm ml-2 flex-1"
-          {...register(name, { required: textMessage, pattern, minLength })}
-          {...props}
-        />
-        {typePassword ? (
-          <span onClick={handleEye}>{eye ? <FaEye /> : <IoEyeOff /> } </span>
-        ) : (
-          ""
-        )}
-      </div>
-      <div
-        style={{
-          textAlign: "start",
-        }}
-      >
-        {errors[name] && (
-          <p style={{ fontSize: "12px", color: "red" }}>
-            {errors[name].message}
-          </p>
-        )}
-        {passwordsMatch && (
-          <p style={{ fontSize: "12px", color: "red" }}>
-            {errors[name] ? null : "Mật khẩu không trùng khớp "}
-          </p>
-        )}
-        {severErorEmail && (
-          <p style={{ fontSize: "12px", color: "red" }}>
-            {errors[name] ? null : `Email đã tồn tại `}
-          </p>
-        )}
-        {severErorPhone && (
-          <p style={{ fontSize: "12px", color: "red" }}>
-            {errors[name] ? null : `Số điện thoại đã tồn tại `}
-          </p>
-        )}
-        {severErorUser && (
-          <p style={{ fontSize: "12px", color: "red" }}>
-            {errors[name] ? null : `User đã tồn tại `}
-          </p>
-        )}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div style={{ alignItems: 'center', border: `0.5px solid ${checkErr()}` }} className="bg-gray-100 p-2 flex">
+                {icon}
+                <input
+                    id="input"
+                    name={name}
+                    type={typePassword ? (eye ? 'text' : 'password') : 'text'}
+                    placeholder={placeholder}
+                    onInput={() => {
+                        handleReset ? handleReset() : null;
+                    }}
+                    className="w_64 bg_transparent bg-gray-100 outline-none text-sm ml-2 flex-1"
+                    {...register(name, { required: textMessage, pattern, minLength })}
+                    {...props}
+                />
+                {typePassword ? <span onClick={handleEye}>{eye ? <FaEye /> : <IoEyeOff />} </span> : ''}
+            </div>
+            <div
+                style={{
+                    textAlign: 'start',
+                }}
+            >
+                {errors[name] && <p style={{ fontSize: '12px', color: 'red' }}>{errors[name].message}</p>}
+                {passwordsMatch && (
+                    <p style={{ fontSize: '12px', color: 'red' }}>
+                        {errors[name] ? null : 'Mật khẩu không trùng khớp '}
+                    </p>
+                )}
+                {severErorEmail && (
+                    <p style={{ fontSize: '12px', color: 'red' }}>{errors[name] ? null : `Email đã tồn tại `}</p>
+                )}
+                {severErorPhone && (
+                    <p style={{ fontSize: '12px', color: 'red' }}>
+                        {errors[name] ? null : `Số điện thoại đã tồn tại `}
+                    </p>
+                )}
+                {severErorUser && (
+                    <p style={{ fontSize: '12px', color: 'red' }}>{errors[name] ? null : `User đã tồn tại `}</p>
+                )}
+            </div>
+        </>
+    );
 }
 
 export default InputComponent;
 InputComponent.defaultProps = {
-  type: "text ",
-  placeholder: "placeholder",
-  textMessage: "textMessage",
-  pattern: null,
+    type: 'text ',
+    placeholder: 'placeholder',
+    textMessage: 'textMessage',
+    pattern: null,
 };
 
 InputComponent.propTypes = {
-  type: PropTypes.string,
-  placeholder: PropTypes.string,
-  textMessage: PropTypes.string,
-  pattern: PropTypes.object,
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    textMessage: PropTypes.string,
+    pattern: PropTypes.object,
 };
 
 // Input Login Field
 export const InputLoginField = ({ Icon, type = 'text', placeholder, value, onChange, error }) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const inputRef = useRef(null);
+    const [showPassword, setShowPassword] = useState(false);
+    const inputRef = useRef(null);
 
-  const handleClick = () => {
-      inputRef.current.focus();
-  };
+    const handleClick = () => {
+        inputRef.current.focus();
+    };
 
-  const togglePasswordVisibility = () => {
-      setShowPassword(!showPassword);
-  };
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
-  return (
-      <div className={`bg-gray-100 w-[80%] p-2 flex items-center mb-3 ${error ? 'border-red-500 border' : ''}`}>
-          <Icon className="text-black m-2" onClick={handleClick} />
-          <input
-              type={type === 'password' ? (showPassword ? 'text' : 'password') : type}
-              placeholder={placeholder}
-              className="bg-gray-100 outline-none text-sm flex-1"
-              value={value}
-              onChange={onChange}
-              ref={inputRef}
-          />
-          {type === 'password' && (
-              <div onClick={togglePasswordVisibility} className="cursor-pointer p-2">
-                  {showPassword ? <FaEye className="text-black" /> : <FaEyeSlash className="text-black" />}
-              </div>
-          )}
-      </div>
-  );
+    return (
+        <div className={`bg-gray-100 w-[80%] p-2 flex items-center mb-3 ${error ? 'border-red-500 border' : ''}`}>
+            <Icon className="text-black m-2" onClick={handleClick} />
+            <input
+                type={type === 'password' ? (showPassword ? 'text' : 'password') : type}
+                placeholder={placeholder}
+                className="bg-gray-100 outline-none text-sm flex-1"
+                value={value}
+                onChange={onChange}
+                ref={inputRef}
+            />
+            {type === 'password' && (
+                <div onClick={togglePasswordVisibility} className="cursor-pointer p-2">
+                    {showPassword ? <FaEye className="text-black" /> : <FaEyeSlash className="text-black" />}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export const InpuProfileField = ({ label, id, value, isEditable, onChange, toggleEdit }) => {
+    return (
+        <div className="col-span-6 sm:col-span-3">
+            {label && (
+                <label htmlFor={id} className="block text-sm font-medium text-gray-700 mb-3">
+                    {label}
+                </label>
+            )}
+            <div className="relative w-full">
+                <input
+                    type="text"
+                    onChange={onChange}
+                    value={value}
+                    readOnly={!isEditable}
+                    id={id}
+                    className={`w-full px-3 py-2 pr-10 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 ${
+                        !isEditable && 'bg-gray-100'
+                    }`}
+                />
+                <button
+                    type="button"
+                    onClick={toggleEdit}
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+                >
+                    <FaPencilAlt />
+                </button>
+            </div>
+        </div>
+    );
 };
--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,9 +1,43 @@
 import React, { useEffect, useState } from 'react';
-// import axios from '~/api/axios';
-// import { useParams, Link } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle, FaPencilAlt } from 'react-icons/fa';
+import { InpuProfileField } from '@/components/constants/Input';
 
-function Profile() {
+export default function ProfilePage() {
+    const [fullName, setFullName] = useState('NightFury');
+    const [tel, setTel] = useState('0123456789');
+    const [email, setEmail] = useState('test@gmail.com');
+    const [address, setAddress] = useState('Việt Nam');
+    const [isEditable, setIsEditable] = useState({});
+
+    // Edit profile
+    const toggleEdit = (field) => {
+        setIsEditable((prev) => ({
+            ...prev,
+            [field]: !prev[field],
+        }));
+    };
+
+    // Save profile
+    const saveProfile = () => {
+        console.log('Đã lưu thông tin');
+        setIsEditable({
+            firstName: false,
+            tel: false,
+            email: false,
+            address: false,
+        });
+    };
+
+    // Cancel edit profile
+    const cancelEdit = () => {
+        console.log('Đã hủy chỉnh sửa');
+        setIsEditable({
+            firstName: false,
+            tel: false,
+            email: false,
+            address: false,
+        });
+    };
     // const { username } = useParams();
     // const GET_USER_URL = `/profile/${username}`;
     // const UPDATE_USER_URL = `/account/${username}/update`;
@@ -72,14 +106,9 @@ function Profile() {
                                 <div className="relative">
                                     <label htmlFor="image">
                                         <div className="top-0 w-32 h-32 inline-flex overflow-hidden absolutes justify-center items-center bg-gray-100 rounded-full dark:bg-gray-600">
-                                            {/* {image && <img src={image} alt="avatar" />}
-                                            {selectedImage && (
-                                                <img alt="avatar" src={URL.createObjectURL(selectedImage)} />
-                                            )} */}
                                             <FaUserCircle size={100} />
                                         </div>
                                     </label>
-                                    {/* {edit ? ( */}
                                     <input
                                         id="image"
                                         type="file"
@@ -90,119 +119,97 @@ function Profile() {
                                             setImage(null);
                                         }}
                                     />
-                                    {/* ) : null} */}
                                 </div>
                                 {/* AVATAR */}
                                 <div className="px-4 sm:px-0">
-                                    {/* Username */}
-                                    <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">
-                                        {/* <Link to={`/profile/${username}`}>@{uname}</Link> */}
-                                        NightFury
-                                    </h2>
-                                    {/* Username */}
-
                                     {/* Name */}
                                     <div className="text-Gray-800 font-bold text-[2rem] flex gap-2 items-center">
-                                        {/* {firstName + ' ' + lastName} */}
                                         NightFury
                                     </div>
-                                    {/* Name */}
-
-                                    {/* Status */}
-                                    <p className="leading-relaxed mb-5">Motorbike Ecommerce</p>
-                                    {/* Status */}
                                 </div>
                             </div>
                             <div className="mt-5 md:col-span-2 md:mt-0">
-                                <form id="updateUser" onSubmit={(e) => handleUpdateUser(e)}>
+                                <form>
                                     <div className="overflow-hidden shadow sm:rounded-md">
                                         <div className="bg-white px-4 py-5 sm:p-6">
                                             <div className="grid grid-cols-6 gap-6">
+                                                {/* FullName Field */}
                                                 <div className="col-span-6 sm:col-span-3">
-                                                    <label
-                                                        htmlFor="firstName"
-                                                        className="block text-sm font-medium text-gray-700"
-                                                    >
-                                                        First Name
-                                                    </label>
-                                                    {/* {edit ? ( */}
-                                                    <input
-                                                        type="text"
-                                                        onChange={(e) => setFirstName(e.target.value)}
-                                                        // value={firstName}
-                                                        id="firstName"
-                                                        className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
-                                                    />
-                                                    {/* ) : ( */}
-                                                    {/* <input
-                                                        type="text"
-                                                        readOnly
-                                                        value={firstName}
-                                                        id="firstName"
-                                                        className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
-                                                    /> */}
-                                                    {/* )} */}
+                                                    <div className="relative w-full">
+                                                        <InpuProfileField
+                                                            label="Họ và Tên"
+                                                            id="fullName"
+                                                            value={fullName}
+                                                            isEditable={isEditable.firstName}
+                                                            onChange={(e) => setFullName(e.target.value)}
+                                                            toggleEdit={() => toggleEdit('firstName')}
+                                                        />
+                                                    </div>
                                                 </div>
+
+                                                {/* Telephone Field */}
                                                 <div className="col-span-6 sm:col-span-3">
-                                                    <label
-                                                        htmlFor="lastName"
-                                                        className="block text-sm font-medium text-gray-700"
-                                                    >
-                                                        Last Name
-                                                    </label>
-                                                    {/* {edit ? ( */}
-                                                    <input
-                                                        type="text"
-                                                        onChange={(e) => setLastName(e.target.value)}
-                                                        // value={lastName}
-                                                        id="lastName"
-                                                        className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
-                                                    />
-                                                    {/* ) : ( */}
-                                                    <input
-                                                        type="text"
-                                                        readOnly
-                                                        // value={lastName}
-                                                        id="lastName"
-                                                        className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
-                                                    />
-                                                    {/* )} */}
+                                                    <div className="relative w-full">
+                                                        <InpuProfileField
+                                                            label="Số điện thoại"
+                                                            id="tel"
+                                                            value={tel}
+                                                            isEditable={isEditable.tel}
+                                                            onChange={(e) => setTel(e.target.value)}
+                                                            toggleEdit={() => toggleEdit('tel')}
+                                                        />
+                                                    </div>
                                                 </div>
+
+                                                {/* Email Field */}
                                                 <div className="col-span-6 sm:col-span-3">
-                                                    <label
-                                                        htmlFor="email"
-                                                        className="block text-sm font-medium text-gray-700"
-                                                    >
-                                                        Email
-                                                    </label>
-                                                    {/* {edit ? ( */}
-                                                    <input
-                                                        type="email"
-                                                        onChange={(e) => setEmail(e.target.value)}
-                                                        // value={email}
-                                                        id="email"
-                                                        className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
-                                                    />
-                                                    {/* ) : ( */}
-                                                    <input
-                                                        type="email"
-                                                        readOnly
-                                                        // value={email}
-                                                        id="email"
-                                                        className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
-                                                    />
-                                                    {/* )} */}
+                                                    <div className="relative w-full">
+                                                        <InpuProfileField
+                                                            label="Email"
+                                                            id="email"
+                                                            value={email}
+                                                            isEditable={isEditable.email}
+                                                            onChange={(e) => setEmail(e.target.value)}
+                                                            toggleEdit={() => toggleEdit('email')}
+                                                        />
+                                                    </div>
+                                                </div>
+
+                                                {/* Address Field */}
+                                                <div className="col-span-6 sm:col-span-3">
+                                                    <div className="relative w-full">
+                                                        <InpuProfileField
+                                                            label="Address"
+                                                            id="address"
+                                                            value={address}
+                                                            isEditable={isEditable.address}
+                                                            onChange={(e) => setAddress(e.target.value)}
+                                                            toggleEdit={() => toggleEdit('address')}
+                                                        />
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="x-4 py-3 text-right sm:px-6">
-                                            <button
-                                                type="submit"
-                                                form="updateUser"
-                                                className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            >
-                                                Save
-                                            </button>
+                                        {/* Nút Lưu và các phần khác */}
+                                        <div className="bg-white px-4 py-5 sm:p-6">
+                                            <div className="grid grid-cols-6 gap-6">
+                                                {/* Các trường nhập thông tin */}
+                                            </div>
+                                            {/* Nút Lưu và Thoát */}
+                                            <div className="flex justify-end mt-4 space-x-4">
+                                                <button
+                                                    onClick={cancelEdit}
+                                                    className="bg-[#D9D9D9] hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-lg"
+                                                >
+                                                    Thoát
+                                                </button>
+                                                <button
+                                                    onClick={saveProfile}
+                                                    className="bg-[#2B92E4] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+                                                >
+                                                    Lưu
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </form>
@@ -214,5 +221,3 @@ function Profile() {
         </section>
     );
 }
-
-export default Profile;
